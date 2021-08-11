536c183d5f77aea7a827b77a3f91b8295d3b2ff9
v-cypher <86663871+v-cypher@users.noreply.github.com>
2021-08-11 21:19:44 +0530

    Update dictonaries_sets.py

--- dictonaries_sets.py
+++ dictonaries_sets.py
@@ -1,10 +1,9 @@
+##############DICTONARY#################
 # Dictionaries store mappings from keys to values
 empty_dict = {}
 # Here is a prefilled dictionary
 filled_dict = {"one": 1, "two": 2, "three": 3}
-
-# Note keys for dictionaries have to be immutable types. This is to ensure that
-# the key can be converted to a constant hash value for quick look-ups.
+#keys are immutable.
 # Immutable types include ints, floats, strings, tuples.
 invalid_dict = {[1,2,3]: "123"}  # => Raises a TypeError: unhashable type: 'list'
 valid_dict = {(1,2,3):[1,2,3]}   # Values can be of any type, however.
@@ -12,24 +11,14 @@ valid_dict = {(1,2,3):[1,2,3]}   # Values can be of any type, however.
 # Look up values with []
 filled_dict["one"]  # => 1
 
-# Get all keys as an iterable with "keys()". We need to wrap the call in list()
-# to turn it into a list. We'll talk about those later.  Note - for Python
-# versions <3.7, dictionary key ordering is not guaranteed. Your results might
-# not match the example below exactly. However, as of Python 3.7, dictionary
 # items maintain the order at which they are inserted into the dictionary.
 list(filled_dict.keys())  # => ["three", "two", "one"] in Python <3.7
 list(filled_dict.keys())  # => ["one", "two", "three"] in Python 3.7+
 
 
 # Get all values as an iterable with "values()". Once again we need to wrap it
-# in list() to get it out of the iterable. Note - Same as above regarding key
-# ordering.
 list(filled_dict.values())  # => [3, 2, 1]  in Python <3.7
 list(filled_dict.values())  # => [1, 2, 3] in Python 3.7+
-
-# Check for existence of keys in a dictionary with "in"
-"one" in filled_dict  # => True
-1 in filled_dict      # => False
 
 # Looking up a non-existing key is a KeyError
 filled_dict["four"]  # KeyError
@@ -41,10 +30,6 @@ filled_dict.get("four")     # => None
 filled_dict.get("one", 4)   # => 1
 filled_dict.get("four", 4)  # => 4
 
-# "setdefault()" inserts into a dictionary only if the given key isn't present
-filled_dict.setdefault("five", 5)  # filled_dict["five"] is set to 5
-filled_dict.setdefault("five", 6)  # filled_dict["five"] is still 5
-
 # Adding to a dictionary
 filled_dict.update({"four":4})  # => {"one": 1, "two": 2, "three": 3, "four": 4}
 filled_dict["four"] = 4         # another way to add to dict
@@ -52,11 +37,7 @@ filled_dict["four"] = 4         # another way to add to dict
 # Remove keys from a dictionary with del
 del filled_dict["one"]  # Removes the key "one" from filled dict
 
-# From Python 3.5 you can also use the additional unpacking options
-{'a': 1, **{'b': 2}}  # => {'a': 1, 'b': 2}
-{'a': 1, **{'a': 2}}  # => {'a': 2}
-
-
+#############SETS################
 
 # Sets store ... well sets
 empty_set = set()
@@ -95,7 +76,4 @@ filled_set | other_set  # => {1, 2, 3, 4, 5, 6}
 # Check for existence in a set with in
 2 in filled_set   # => True
 10 in filled_set  # => False
-
-# Make a one layer deep copy
-filled_set = some_set.copy()  # filled_set is {1, 2, 3, 4, 5}
-filled_set is some_set        # => False
+##################END###############################VK##############
